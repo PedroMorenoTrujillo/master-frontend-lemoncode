390b0e7a63a5d3f378fce517f611153d021294ae
Pedro Moreno Trujillo <pedro.moreno.trujillo@gmail.com>
2024-12-17 18:01:06 +0100

    added bolier plate folder for testing exercise

--- 04-frameworks/React/intermediate/intermediate-react/src/App.tsx
+++ 04-frameworks/React/intermediate/intermediate-react/src/App.tsx
@@ -3,7 +3,7 @@ import { AppLayout, MainLayout } from "./layout";
 import { Cart } from "./scenes";
 import { PictureProvider } from "./core/Picture";
 import { RouterComponent } from "./core/router";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 export const App: React.FC = () => {
   return (
